c677d01708c97107616552828136c5935e4386d6
sc-parivesh <parivesh.softcoded@gmail.com>
2024-07-27 17:24:30 +0530

    test

--- MUI/src/commponents/PracticeModal.jsx
+++ MUI/src/commponents/PracticeModal.jsx
@@ -41,6 +41,7 @@ const PracticeModal = () => {
             </Typography>
             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
               Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              jshgdhfhgafsdhfh
             </Typography>
           </Box>
         </Modal>
